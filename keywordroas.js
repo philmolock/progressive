62e0086238abf3db13cc93b7d7c0b3345a21a06c
philmolock <47786752+philmolock@users.noreply.github.com>
2019-04-17 09:00:21 -0500

    Update keywordroas.js

--- keywordroas.js
+++ keywordroas.js
@@ -1,1 +1,21 @@
-// first commit
+/**
+ * BING ADS SAMPLE SCRIPT NOT INTENDED FOR PRODUTION USE
+ * ONLY USE THIS SCRIPT IN PREVIEW MODE; IF YOU RUN THIS SCRIPT IT WILL MODIFY YOUR BIDS
+ * PLEASE SEE THE BING ADS SCRIPT DOCUMENTAITON FOR MORE INFORMATION: https://docs.microsoft.com/en-us/bingads/scripts/
+ */
+
+function main() {
+    let keywords = BingAdsApp.keywords().forDateRange("LAST_14_DAYS").get();
+    let bidModifier = 2;
+
+    while(keywords.hasNext()){
+        let keyword = keywords.next();
+        let roas = keyword.getStats().getReturnOnAdSpend();
+        
+        if(roas >= 0){
+            let currentCPC = keyword.bidding().getCpc();
+            let newCPC = currentCPC * bidModifier;
+            keyword.bidding().setCpc(newCPC);
+        }
+    }
+}
